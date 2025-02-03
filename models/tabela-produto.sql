706f3c8f359c6981b23dec90118f1e54b21a2572
Felipe Pegoraro <felipegoraro@outlook.com.br>
2025-02-03 16:47:15 +0000

    ajustando comentario tabela produto

--- models/tabela-produto.sql
+++ models/tabela-produto.sql
@@ -2,5 +2,5 @@ SELECT
     id,    -- Seleciona a coluna 'id'
     titulo -- Seleciona a coluna 'titulo'
 
-FROM {{ source("nintendo_projeto_dev", "nintendo-bigtable") }} -- Fonte de dados: tabela "nintendo-bigtable" no projeto "nintendo_projeto_prd"
+FROM {{ source("nintendo_projeto_dev", "nintendo-bigtable") }} -- Fonte de dados: tabela "nintendo-bigtable" no projeto "nintendo_projeto_dev"
 ORDER BY id        -- Ordena os resultados pela coluna 'id'
